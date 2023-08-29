595c4506ce13bdbaae08452609ff38db73122a2a
Aliasgar Merchant <amercha1@stevens.edu>
2023-08-29 14:39:20 +0200

    Create new vector store/index in Pinecone

--- src/pinecone.js
+++ src/pinecone.js
@@ -1,20 +1,20 @@
 //This file will handle initializing Pinecone, creating the index, inserting vectors, and any other Pinecone related operations.
 
 const Pinecone = require('pinecone-client');
-
 const pinecone = new Pinecone(process.env.PINECONE_API_KEY);
 
-function createIndex(indexName) {
-    return pinecone.createIndex(indexName, "cosine");
-}
-
-function insertVectors(indexName, vectors) {
-    // ... your insertion logic
+//Initialize Pinecone Vectore Store 
+async function initializeVectoreStore(indexName) {
+    try {
+        //Create an index in Pinecone
+        await pinecone.createIndex(indexName, "cosine");
+        console.log("Successfully initialized pinecone vectore store with name :${indexName}");
+    } catch (error) {
+        console.error("Error initializing Pinecone vector store:", error);
+    }
 }
 
 // Export the functions for use in other files
 module.exports = {
-    createIndex,
-    insertVectors,
-    // ... any other functions
+    initializeVectoreStore
 };
