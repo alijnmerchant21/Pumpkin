c75df54ebab876758723d69dce1044de786fc02c
Aliasgar Merchant <amercha1@stevens.edu>
2023-08-29 14:42:49 +0200

    Read all .txt and .md file and return their content in array

--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,12 @@ const path = require('path');
 
 function readDocuments(basePath) {
     let documents = [];
-    // ... your reading logic
+    fs.readdirSync(basePath).forEach(file => {
+        if(file.endsWith('.txt') || file.endsWith('.md')) {
+            const content = fs.readFileSync(path.join(basePath, file), 'utf-8');
+            documents.push(content);
+        }
+    });
     return documents;
 }
 
