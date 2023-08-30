5f0bd99e6d24ec51050ae9f36f3d826e15981ac3
Aliasgar Merchant <amercha1@stevens.edu>
2023-08-30 13:32:33 +0200

    Load all documents of type txt, etc

--- src/documentLoader.js
+++ src/documentLoader.js
@@ -1,26 +1,21 @@
-// This module will handle the chat functionality using the ConversationalRetrievalQAChain.
+// This module will load and retrieve the document from the specified URL.
 
-import dotenv from 'dotenv';
-import { ConversationalRetrievalQAChain } from "langchain/chains";
-import { BufferMemory } from "langchain/memory";
-import { ChatOpenAI } from "langchain/chat_models/openai";
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+import { JSONLoader, JSONLinesLoader } from "langchain/document_loaders/fs/json";
+import { TextLoader } from "langchain/document_loaders/fs/text";
+import { CSVLoader } from "langchain/document_loaders/fs/csv";
 
-dotenv.config();
+export async function loadAllDocuments() {
+  const loader = new DirectoryLoader(
+    "src/document_loaders/example_data/example",
+    {
+      ".json": (path) => new JSONLoader(path, "/texts"),
+      ".jsonl": (path) => new JSONLinesLoader(path, "/html"),
+      ".txt": (path) => new TextLoader(path),
+      ".csv": (path) => new CSVLoader(path, "text"),
+    }
+  );
 
-export async function initChatBot(vectorStore) {
-  const memory = new BufferMemory({
-    memoryKey: "chat_history",
-    returnMessages: true,
-  });
-
-  const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo", apiKey: process.env.OPENAI_API_KEY });
-  const chain = ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), { memory });
-
-  return chain;
+  const docs = await loader.load();
+  return docs;
 }
-
-export async function chat(chain, question) {
-  const result = await chain.call({ question });
-  return result;
-}
-
