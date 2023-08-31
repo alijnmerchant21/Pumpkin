d63940df617d30f76c85b53f6d51c2e2018ab925
Aliasgar Merchant <amercha1@stevens.edu>
2023-08-31 11:38:04 +0200

    Add source of info
    Temporarily remove memory for debugging

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,7 +1,8 @@
 // This module will handle the chat functionality using the ConversationalRetrievalQAChain.
 
 import dotenv from 'dotenv';
-import { ConversationalRetrievalQAChain } from "langchain/chains";
+//import { ConversationalRetrievalQAChain } from "langchain/chains";
+import { RetrievalQAChain } from "langchain/chains";
 import { BufferMemory } from "langchain/memory";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 
@@ -9,18 +10,30 @@ dotenv.config();
 
 export async function initChatBot(vectorStore) {
   const memory = new BufferMemory({
-    memoryKey: "chat_history",
+    // memoryKey: "chat_history",
     returnMessages: true,
+    outputKey: "response"
   });
 
   const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo", apiKey: process.env.OPENAI_API_KEY });
-  const chain = ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), { memory });
+  //const chain = ConversationalRetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), { memory });
+  const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever(), { returnSourceDocuments: true } );
+
 
   return chain;
 }
 
 export async function chat(chain, question) {
-  const result = await chain.call({ question });
-  return result;
-}
+  const result = await chain.call({ query: question });
 
+  // Attempt to extract the file name from the top of the source document content
+  const fileNameRegex = /\[FileName: (.+?)\]/;
+  const sources = result.sourceDocuments.map(doc => {
+    const matches = fileNameRegex.exec(doc.pageContent);
+    return matches && matches[1];
+  }).filter(Boolean);
+  
+const responseWithSource = `${result?.text} (Sources: ${sources.join(', ')})`;
+
+  return responseWithSource;
+}
